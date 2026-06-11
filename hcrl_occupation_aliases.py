780dcb3dda50e2fdd6ae49184415bf484524d66c
SaidAmir-dev <su1763@rit.edu>
2026-06-11 12:28:20 -0400

    Create hcrl_occupation_aliases.py

---
+++ hcrl_occupation_aliases.py
@@ -0,0 +1,52 @@
+"""
+Enterprise occupation alias dictionary.
+Maps common company job titles to O*NET occupations.
+"""
+
+OCCUPATION_ALIASES = {
+
+    # Sales
+    "sales executive": "Sales Managers",
+    "sales representative": "Sales Representatives, Wholesale and Manufacturing, Except Technical and Scientific Products",
+
+    # Research
+    "research scientist": "Materials Scientists",
+    "scientist": "Materials Scientists",
+
+    # Manufacturing
+    "manufacturing director": "Industrial Production Managers",
+    "production director": "Industrial Production Managers",
+
+    # HR
+    "human resources": "Human Resources Managers",
+    "hr manager": "Human Resources Managers",
+    "human resources manager": "Human Resources Managers",
+
+    # Healthcare
+    "healthcare representative": "Medical and Health Services Managers",
+
+    # Management
+    "manager": "General and Operations Managers",
+
+    # Finance
+    "finance manager": "Financial Managers",
+    "financial manager": "Financial Managers",
+
+    # Operations
+    "operations manager": "General and Operations Managers",
+
+    # IT
+    "software engineer": "Software Developers",
+    "data scientist": "Data Scientists",
+    "data analyst": "Data Analysts",
+
+    # Customer Service
+    "customer service representative": "Customer Service Representatives",
+
+    # Laboratory
+    "laboratory technician": "Dental Laboratory Technicians",
+
+    # Engineering
+    "engineering manager": "Architectural and Engineering Managers",
+    "engineer": "Engineers"
+}
